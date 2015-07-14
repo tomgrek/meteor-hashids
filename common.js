7c1a6a677768a1c167437b9432057473ff320e09
Tim Brandin <tim.brandin@gmail.com>
2015-07-14 09:44:37 +0200

    Updated hashid version.

--- common.js
+++ common.js
@@ -1,9 +1,9 @@
-//if (typeof Package !== "undefined") Hashids = this.Hashids;
+Hashids = Npm.require('hashids');
 
 _.extend(Meteor, {
   hashid: function() {
     var hash, uuid;
-    uuid = new Hashids(Meteor.uuid());
-    return hash = uuid.encrypt(Date.now());
+    uuid = new Hashids(Meteor.uuid(), 6, 'ABCDEFGHIJKLMNOPQRSTUVXYZ123456789');
+    return hash = uuid.encode(Date.now());
   }
 });
--- package.js
+++ package.js
@@ -1,12 +1,23 @@
 Package.describe({
-  summary: "A small JavaScript class to generate YouTube-like hashes from one or many numbers. This is a client-side version of Node.js hashids"
+  name: 'timbrandin:hashids',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'A small JavaScript class to generate YouTube-like hashes from one or many numbers. This is a client-side version of Node.js hashids',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
 });
 
-Package.on_use(function (api, where) {
-  where = where || ['client', 'server'];
+Npm.depends({
+  "hashids": "1.0.2"
+});
 
-  api.use('underscore', where);
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
 
-  api.add_files('hashids.min.js', where);
-  api.add_files('common.js', where);
+  api.use('underscore');
+
+  api.addFiles('common.js', "server");
 });
