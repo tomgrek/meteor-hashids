259593a04e0031f29a781d3f6fed508a8b6e8d92
Tim Brandin <tim.brandin@gmail.com>
2015-07-14 09:45:18 +0200

    Version bump.

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'timbrandin:hashids',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'A small JavaScript class to generate YouTube-like hashes from one or many numbers. This is a client-side version of Node.js hashids',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/timbrandin/meteor-hashids',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
