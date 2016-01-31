f812180b8568369c695a88e855ea0a2013553157
Tom Grek <tom.grek@gmail.com>
2016-01-31 12:48:56 +0700

    Update package.js

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'timbrandin:hashids',
+  name: 'tomgrek:hashids',
   version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Hashids made simple for Meteor applications',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/timbrandin/meteor-hashids',
+  git: 'https://github.com/tomgrek/meteor-hashids',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
