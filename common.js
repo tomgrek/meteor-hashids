14d13ba75177c84dc24a63757eb6838fcfc6d46f
Tom Grek <tom.grek@gmail.com>
2016-01-31 13:08:45 +0700

    Update common.js

--- common.js
+++ common.js
@@ -1,11 +1,6 @@
 Hashids = Npm.require('hashids');
 
 _.extend(Meteor, {
-  hashid: function() {
-    var hash, uuid;
-    uuid = new Hashids(Meteor.uuid(), 0, 'abcdefghjkmnpqrstxy1234567890');
-    return hash = uuid.encode(Date.now());
-  },
   hashid: function(param) {
     var hash, uuid;
     uuid = new Hashids(Meteor.uuid(), 0, 'abcdefghjkmnpqrstxy1234567890');
