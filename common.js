d3f666c774a3322d08b4f11cffb49676e3cf5017
Tom Grek <tom.grek@gmail.com>
2016-01-31 12:48:05 +0700

    shortest possible hash and more friendly chars

--- common.js
+++ common.js
@@ -3,7 +3,7 @@ Hashids = Npm.require('hashids');
 _.extend(Meteor, {
   hashid: function() {
     var hash, uuid;
-    uuid = new Hashids(Meteor.uuid(), 6, 'ABCDEFGHIJKLMNOPQRSTUVXYZ123456789');
+    uuid = new Hashids(Meteor.uuid(), 0, 'abcdefghjkmnpqrstxy1234567890');
     return hash = uuid.encode(Date.now());
   }
 });
